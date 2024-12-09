ed516a17bfce68b8bcc17ba4b604ef30b773de5f
AndK37 <146865715+AndK37@users.noreply.github.com>
2024-12-09 18:47:03 +0500

    final build

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,18 +1,18 @@
 <template>
     <div>
-        <card-item img_path="/categories/image1.png" name="Стекло"></card-item>
-        <card-item img_path="/categories/image2.png" name="Мягкая кровля и гидроизоляция"></card-item>
-        <card-item img_path="/categories/image3.png" name="Битумы"></card-item>
-        <card-item img_path="/categories/image4.png" name="Стеклопакеты"></card-item>
-        <card-item img_path="/categories/image5.png" name="Электроды"></card-item>
-        <card-item img_path="/categories/image6.png" name="Круги отрезные, зачистные"></card-item>
+        <card-item img_path="categories/image1.png" name="Стекло"></card-item>
+        <card-item img_path="categories/image2.png" name="Мягкая кровля и гидроизоляция"></card-item>
+        <card-item img_path="categories/image3.png" name="Битумы"></card-item>
+        <card-item img_path="categories/image4.png" name="Стеклопакеты"></card-item>
+        <card-item img_path="categories/image5.png" name="Электроды"></card-item>
+        <card-item img_path="categories/image6.png" name="Круги отрезные, зачистные"></card-item>
 
-        <card-item img_path="/categories/image7.png" name="Парафин"></card-item>
-        <card-item img_path="/categories/image8.png" name="Мастики, Праймер, Грунтовки, Клей и Герметики"></card-item>
-        <card-item img_path="/categories/image9.png" name="Утеплители, пароизоляция, пленка"></card-item>
-        <card-item img_path="/categories/image10.png" name="Стеклоблок"></card-item>
-        <card-item img_path="/categories/image11.png" name="Гвозди"></card-item>
-        <card-item img_path="/categories/image12.png" name="Алмазный инструмент"></card-item>
+        <card-item img_path="categories/image7.png" name="Парафин"></card-item>
+        <card-item img_path="categories/image8.png" name="Мастики, Праймер, Грунтовки, Клей и Герметики"></card-item>
+        <card-item img_path="categories/image9.png" name="Утеплители, пароизоляция, пленка"></card-item>
+        <card-item img_path="categories/image10.png" name="Стеклоблок"></card-item>
+        <card-item img_path="categories/image11.png" name="Гвозди"></card-item>
+        <card-item img_path="categories/image12.png" name="Алмазный инструмент"></card-item>
     </div>
 
 </template>
